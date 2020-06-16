f22f0591737351232e5980ac14d7088a1f885042
dermoagenda <josemauromani@gmail.com>
2020-06-16 17:18:44 -0300

    product create component

---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,37 @@
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name:'Produto de Teste',
+    price: 150.45
+  }
+
+  constructor(
+    private productService: ProductService,
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(()=>{
+      this.productService.showMessage('Produto criado')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+  
+}
