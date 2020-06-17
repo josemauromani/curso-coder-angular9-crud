15cc1bfcde9bde566206e7f5be255950e552b50c
dermoagenda <josemauromani@gmail.com>
2020-06-17 08:28:18 -0300

    initial product list

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 @NgModule({
   declarations: [ // componentes, diretivas e pipes
@@ -36,6 +37,7 @@ import { MatInputModule } from '@angular/material/input';
     ProductCrudComponent,
     ProductCreateComponent,
     RedDirective,
+    ProductReadComponent,
   ],
   imports: [
     BrowserModule,
