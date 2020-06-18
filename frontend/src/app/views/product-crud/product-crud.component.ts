9a8d12ae038a42ddf35324715833a5e67a972be4
dermoagenda <josemauromani@gmail.com>
2020-06-18 14:50:28 -0300

    header data

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
@@ -7,8 +8,12 @@ import { Router } from '@angular/router';
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor(private router: Router) {
-
+  constructor(private router: Router,private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routerUrl: '/products'
+    }
   }
 
   ngOnInit(): void {
